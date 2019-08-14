af7ff9de8515b2be20b3068d5e7a7c2e852a07a3
MakSim345 <senishch@gmail.com>
2019-08-14 16:16:16 +0300

    edu VS project for pointers used for setter/getter in private class methods

---
+++ pointers_vs/mainc.cpp
@@ -0,0 +1,132 @@
+#include "gen.h"
+#include "utils.h"
+
+class Programmers
+{
+public:
+    Programmers()
+    {
+
+    }
+
+    Programmers(int iWeightP, int iAgeP)
+    {
+        this->weight = iWeightP;
+        this->age = iAgeP;
+    }
+
+    // simply get references to value created in main and change them
+    void getParams(int &getWeightP, int &getAgeP)
+    {
+        getWeightP = weight;
+        getAgeP = age;
+    }
+
+private:
+    int weight;
+    int age;
+};
+
+
+long g_lTimeTick = 0;
+
+int main(int argc, char *argv[], char *envp[])
+{
+    long LastTimeIn=0;
+    long LastTimeInMS = 0;
+    int RValue = false;
+    int Counter=0;
+    int CaptureModeHex = 0;
+    int NewChar=0;
+    int nResult = 0;
+
+    printf ("main - start\n");
+
+#ifdef FILE_OUTPUT
+    int nOpenFileToken = 0;
+    char *FileNameF = "TimeSt.log";
+    FILE *fi = NULL;
+
+    // open the file for input:
+    if ( (fi = fopen(FileNameF, "a+") ) != NULL)
+    {
+        // printf ("file %s was open succesfully\n", FileNameF);
+        nOpenFileToken = 1;
+    }
+    else
+    {
+        printf ("file %s was not open\n", FileNameF);
+        getchar(); // pause, xyLe...
+        nOpenFileToken = 0;
+    }
+
+    fprintf(fi, "Start time: %d\n", Get1msTime());
+#endif// FILE_OUTPUT
+
+    const int ARR_SIZE = 9;
+    Programmers *prog [ARR_SIZE];
+    Programmers *ptr = nullptr;
+
+    for (int i=0; i < ARR_SIZE; i++)
+    {
+        ptr = new Programmers(i + 100, i);
+        prog[i] = ptr;
+    }
+
+    int w, a;
+    int count = ARR_SIZE;
+
+    //create pointer to pointer,
+    //array's name ("prog") is a pointer to it's first element
+    Programmers **iter = prog;
+
+    for (int i=0; i < count; i++)
+    {
+        ptr = *iter++;
+        ptr->getParams(w, a);
+        if( *(iter-1) != nullptr )
+        {
+            delete *(iter-1);
+            ptr = nullptr;
+        }
+        cout << w << "\t" << a << endl;
+    }
+
+//*************MAIN LOOP*****************//
+ do
+ {
+   //This is simple Windows way:
+   Sleep(1000);
+
+   nResult = produceRND();
+   printf ("1-Time: %ld. RND: %d\n",Get1msTimeMS(), nResult);
+
+   nResult = produceRND();
+   printf ("2-Time: %ld. RND: %d\n",Get1msTimeMS(), nResult);
+
+  // control of endless loop (may be also in monitor.cpp)
+    if (_kbhit())  // has anything been pressed from keyboard ?
+    {
+      RValue = true; // END mark
+    }
+
+
+}while (!RValue);
+
+#ifdef FILE_OUTPUT
+ if (1 == nOpenFileToken)
+ {
+   fclose (fi);
+ }
+#endif
+
+printf ("Application complete.\n");
+
+system ("PAUSE"); // wait for press any key in VS mode
+
+return 0;
+
+}
+
+
+
---
+++ pointers_vs/utils.h
@@ -0,0 +1,34 @@
+#pragma once
+/*********************************************************************
+
+  utils.h  
+  
+**********************************************************************/
+
+void AddTimeStamp(int *pos,char * msg);
+void AddStringPan(char * destStr, char * strToAdd,int len);
+void AddShortTimeStamp(int *pos,char * msg);
+
+int RND_Array(int);
+long  Get1msTime(void);
+long  Get1msTimeMS(void);
+char* PrintTime(void); // return string with time
+void PrintIntroduction();
+void SendStr(char * SendString, int dest, int length);
+
+int produceRND();
+
+class CTimer
+{
+public:
+    CTimer();
+	~CTimer();
+    void Start();
+    unsigned int GetElapsedTimeMs() const;
+	unsigned __int64 GetElapsedTimeMks() const; 
+    
+private:
+    LARGE_INTEGER m_liFreq;
+    LARGE_INTEGER m_liStart;
+    LARGE_INTEGER m_liEnd;
+};
