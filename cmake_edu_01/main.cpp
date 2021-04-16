066e2e4074369028709c10ad83a0714521c9709a
Yuriy Senishch <yuriy.senishch@ge.com>
2021-04-16 09:55:10 +0300

    edu CMAKE and ninja build system - 01

---
+++ cmake_edu_01/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdint.h>
+#include "utils.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    std::cout << std::endl;
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
+
