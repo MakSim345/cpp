763565433e5a5733520c57b560d8adea8a9e6242
Yuriy Senishch <yuriy.senishch@ge.com>
2021-08-23 15:50:24 +0300

    clean up the code

--- virtual_classes_03_edu_vs/virtual.h
+++ virtual_classes_03_edu_vs/virtual.h
@@ -2,45 +2,57 @@
 
 #include "gen.h"
 
-class classAbstractBase 
+class classAbstractBase
 { // Abstract class
 public:
     classAbstractBase ( void ) { std::cout << "Init: object of the classAbstractBase.\n"; }
 
     virtual void Abstr ( void ) = 0; // Pure virtual function
-    void         fun   ( void ) { std::cout << "No realization will inherit!\n"; }
+
+    void fun( void )
+    {
+        std::cout << "No realization will inherit!\n";
+    }
 
     ~classAbstractBase () { std::cout << "classAbstractBase call destructor.\n"; }
 };
 
-class classDerivedAbstract : public classAbstractBase 
+class classDerivedAbstract : public classAbstractBase
 {
 public:
     classDerivedAbstract ( void ) { std::cout << "Init object of the classDerivedAbstract!\n"; }
 
-    void Abstr ( void ){ std::cout << " call function classDerivedAbstract.Abstr();\n"; } //function implementation
-    void fun   ( void ){ std::cout << " call function classDerivedAbstract.fun()\n"; }
+    void Abstr( void )
+    {
+        std::cout << " call function classDerivedAbstract.Abstr();\n";
+    }
+    void fun ( void )
+    {
+        std::cout << " call function classDerivedAbstract.fun()\n";
+    }
 
-    //~CB () {} // Неверно для абстр. кл. ~CB(){ ~CA(); } 
     ~classDerivedAbstract () { std::cout << "classDerivedAbstract call destructor.\n"; }
 };
 
 class baseClassA
 {
 public:
-    baseClassA() 
+    baseClassA()
     {
         data = 10;
+        std::cout << "baseClassA c-tor. data=" << data << std::endl;
     }
 
     virtual void set()
     {
-        std::cout << "baseClassA is increasing" << std::endl;
+        std::cout << "baseClassA increase data..." << std::endl;
         data++;
     }
 
     int get()
     {
+        // ATTN: in case 'get()' called from derived class,
+        // set will be called from that class
         set();
         return data;
     }
@@ -52,27 +64,33 @@ protected:
 class derivedClassB : public baseClassA
 {
 public:
-    void set()
+    derivedClassB()
     {
-        std::cout << "derivedClassB is decreasing" << std::endl;
+        std::cout << "derivedClassB c-tor. data=" << data << std::endl;
+    }
+
+    void set() override
+    // void Set() // without 'override' compiler cannot see the typo!
+    {
+        std::cout << "derivedClassB decrease data..." << std::endl;
         data--;
     }
 };
 
 
-class baseClassFoo 
+class baseClassFoo
 {
 public:
-    virtual void Foo (int n = 10) 
+    virtual void Foo (int n = 10)
     {
         cout << "baseClassFoo::Foo, n = " << n << endl;
     }
 };
 
-class derivedClassFoo : public baseClassFoo 
+class derivedClassFoo : public baseClassFoo
 {
 public:
-    virtual void Foo (int n = 20) 
+    virtual void Foo (int n = 20)
     {
         cout << "derivedClassFoo::Foo, n = " << n << endl;
     }
