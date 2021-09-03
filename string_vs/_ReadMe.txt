9b6c702ab9c19aa09bba3269f1aa0b3de6db4d37
Yuriy Senishch <yuriy.senishch@ge.com>
2021-09-03 10:41:53 +0300

    String edu project for VS2019

---
+++ string_vs/_ReadMe.txt
@@ -0,0 +1,3 @@
+SEP-2021
+
+A string test project in Visual Studio 2019
---
+++ string_vs/main.cpp
@@ -0,0 +1,126 @@
+/* Computes and prints n! for a given n.
+   Uses several basic elements of C++.
+*/
+
+// #include <stdio>
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+static std::atomic<int> av;
+static int gv;
+
+using String = std::string;
+
+class Entity
+{
+public:
+    Entity() : m_Name("Unknown") {};
+    Entity(const String& nameP) : m_Name(nameP) {};
+
+    // ~Entity();
+    const String& GetName() const { return m_Name; }
+
+private:
+    String m_Name;
+};
+
+void hello()
+{
+    std::cout <<
+        "Hello world, I'm a thread!"
+        << std::endl;
+}
+
+static void entry()
+{
+    for (auto i = 0; i < 1000; i++)
+    {
+        // Atomic increment
+        ++av;
+
+        // unsafe code alert
+        ++gv;
+
+        // cout << "thread: av=" << av << " gv=" << gv << endl;
+    }
+}
+
+
+void calculate_factorial()
+{
+    // Declarations of variables
+    int factArg = 0;
+    long fact(1.0);
+    do
+    {
+        cout << "Factorial of: ";
+        cin >> factArg;
+        if (factArg < 0)
+        {
+            cout << "No negative values, please!" << endl;
+        }
+    } while (factArg < 0);
+    int i = 2;
+    while (i <= factArg)
+    {
+        fact = fact * i;
+        i = i + 1;
+    }
+    cout << "The Factorial of " << factArg << " is: " << fact << endl;
+}
+/*
+int main(int argc, char* argv[])
+{
+  boost::thread thrd(&hello);
+  thrd.join();
+  return 0;
+}
+*/
+
+int GetRandom(int maxP)
+{
+    static bool isFirstTime = true;
+    if (isFirstTime)
+    {
+        srand(time(NULL));
+        isFirstTime = false;
+    }
+    auto retVal = rand() % maxP;
+    return retVal;
+}
+
+
+
+int main()
+{
+    std::thread ta(entry);
+    std::thread tb(entry);
+    ta.join();
+    tb.join();
+    
+    // assert(static_cast<int>(av) != gv);
+    // cout << "av=" << av << " gv=" << gv << endl;
+
+    std::string name0 = "Cherno"s + "hello";
+    /*
+    for (int i = 0; i < 10000; i++)
+    {
+        cout << "RND=" << GetRandom(100) << "  ";
+    }
+    cout << endl;
+    */
+    Entity* e;
+    {
+        Entity* entity = new Entity("Cherno");
+        e = entity;
+        std::cout << entity->GetName() << std::endl;
+    }
+    
+    system("PAUSE");
+    return 0;
+}
