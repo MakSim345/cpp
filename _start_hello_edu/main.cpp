24e49a3e7555fc008b2171151da04ee4e86b3426
MakSim345 <senishch@gmail.com>
2023-04-14 13:25:57 +0300

    update cmake file to C++ 17 and remove auto_ptr

--- _start_hello_edu/main.cpp
+++ _start_hello_edu/main.cpp
@@ -10,9 +10,9 @@
 
 using namespace std;
 
-inline float cube(const float s) {return s*s*s;}
+// inline float cube(const float s) {return s*s*s;}
 
-class App
+class ExampleApp
 {
 public:
     int Run()
@@ -20,7 +20,7 @@ public:
         std::cout << "Hello cruel world!\n";
         return 0;
     }
-    ~App()
+    ~ExampleApp()
     {
         std::cout << "Good bye!\n";
     }
@@ -38,39 +38,40 @@ public:
     {
         std::cout << "Good bye!\n";
     }
-    
+
     int run()
     {
         std::cout << "Enter cube dimention: ";
-        std::cin >> side;
-        
+        std::cin >> cubeSideP;
+
         long a = std::clock();
+
+        std::cout << "Cube with side " << cubeSideP << " is equal to " << calcCube(cubeSideP) << "\n";
+
         std::cout << "Time Start: " << a << "\n";
-/*
+
         for (int i = 0; i < 1000000; i++)
         {
-            long _number = my_rnd->getRandomNumber(100, 10000000);
-            int _lenght_name =  my_rnd->getRandomNumber(3, 7);
-            std::string _name = my_name->genPass(_lenght_name);
-
-            pb->add_data_to_file(_name, _number);
+            // std::cout << i << "\n";
+            calcCube(i);
+            lastParamCtr = i;
         }
-  */      
-
-        cout << "Cube with side " << side << " is equal to " << calculation(side) << "\n";
         
+        std::cout << "Cube with side " << lastParamCtr << " is equal to " << calcCube(lastParamCtr) << "\n";
+
         long b = std::clock();
         std::cout << "Time END: " << b << "\n\n";
-        std::cout << "Time TOTAL: " << (b-a)/1000.0 << " sec.\n";    
+        std::cout << "Time TOTAL: " << (b-a)/1000.0 << " sec.\n";
         return 1;
     }
 
 private:
-    float side; 
+    float cubeSideP;
+    long lastParamCtr;
 
-    float calculation(float s)
+    float calcCube(float sideP)
     {
-        return s*s*s;
+        return sideP * sideP * sideP;
     }
 
 };
@@ -78,7 +79,7 @@ private:
 int main()
 {
     // CubeCalc *cube_calc = new CubeCalc();
-    std::auto_ptr <CubeCalc> cube_calc(new CubeCalc());
-    cube_calc->run();    
+    std::unique_ptr <CubeCalc> cube_calc(new CubeCalc());
+    cube_calc->run();
     return 1;
 }
